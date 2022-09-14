2bdeacb5c29ebe2263c137cb3e6cbd71fd339996
HedravBands <hr19ut@brocku.ca>
2022-09-14 02:03:43 -0400

    go_router nav 3 pages from pub.dev

---
+++ lib/src/features/feat1/page1_screen.dart
@@ -0,0 +1,26 @@
+// The screen of the first page.
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../main.dart';
+
+class Page1Screen extends StatelessWidget {
+  /// Creates a [Page1Screen].
+  const Page1Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text(App.title)),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () => context.go('/page2'),
+                child: const Text('Go to page 2'),
+              ),
+            ],
+          ),
+        ),
+      );
+}
---
+++ lib/src/features/feat2/page2_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../main.dart';
+
+/// The screen of the second page.
+class Page2Screen extends StatelessWidget {
+  /// Creates a [Page2Screen].
+  const Page2Screen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(title: const Text(App.title)),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () => context.go('/'),
+                child: const Text('Go back to home page'),
+              ),
+            ],
+          ),
+        ),
+      );
+}
