b427fcc6271a35a822a0675b9ae96807a4fa3cc7
HedravBands <hr19ut@brocku.ca>
2022-09-14 03:28:19 -0400

    go_router apart on .dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:soulflow/src/common/auth/ui/auth_screen.dart';
-import 'package:soulflow/src/common/game/ui/game_screen.dart';
 
-import 'src/common/aurora/10-splash/ui/splash_screen.dart';
-import 'src/common/aurora/20-load/ui/loading_screen.dart';
+import 'src/common/routes/go_router.dart';
 
 void main() {
   runApp(App());
@@ -13,40 +9,15 @@ void main() {
 class App extends StatelessWidget {
   App({super.key});
 
-  static const String title = 'GoRouter: Declarative Routes';
+  static const String title = 'GoRouter';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routeInformationProvider: _router.routeInformationProvider,
-      routeInformationParser: _router.routeInformationParser,
-      routerDelegate: _router.routerDelegate,
+      routeInformationProvider: router.routeInformationProvider,
+      routeInformationParser: router.routeInformationParser,
+      routerDelegate: router.routerDelegate,
       title: title,
     );
   }
-
-  final GoRouter _router = GoRouter(
-    routes: <GoRoute>[
-      GoRoute(
-        path: '/',
-        builder: (BuildContext context, GoRouterState state) =>
-            const SplashScreen(),
-        routes: <GoRoute>[],
-      ),
-      GoRoute(
-        path: '/loading',
-        builder: (BuildContext context, GoRouterState state) =>
-            const LoadingScreen(),
-      ),
-      GoRoute(
-        path: '/auth',
-        builder: (BuildContext context, GoRouterState state) =>
-            const AuthScreen(),
-      ),
-      GoRoute(
-        path: '/game',
-        builder: (BuildContext context, GoRouterState) => const GameScreen(),
-      ),
-    ],
-  );
 }
---
+++ lib/src/common/routes/go_router.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../aurora/10-splash/ui/splash_screen.dart';
+import '../aurora/20-load/ui/loading_screen.dart';
+import '../auth/ui/auth_screen.dart';
+import '../game/ui/game_screen.dart';
+
+final GoRouter router = GoRouter(
+  routes: <GoRoute>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) =>
+          const SplashScreen(),
+      routes: <GoRoute>[],
+    ),
+    GoRoute(
+      path: '/loading',
+      builder: (BuildContext context, GoRouterState state) =>
+          const LoadingScreen(),
+    ),
+    GoRoute(
+      path: '/auth',
+      builder: (BuildContext context, GoRouterState state) =>
+          const AuthScreen(),
+    ),
+    GoRoute(
+      path: '/game',
+      builder: (BuildContext context, GoRouterState) => const GameScreen(),
+    ),
+  ],
+);
